36d656edf46430bc6e00677c652cdeb987ae4520
d4viss <davisonju26@gmail.com>
2023-09-11 19:59:29 -0500

    configuracion de la base de datos

---
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -0,0 +1,23 @@
+package med.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+
+    private String calle, distrito, ciudad, complemento, numero;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.distrito();
+        this.complemento = direccion.complemento();
+        this.numero = direccion.numero();
+    }
+}
