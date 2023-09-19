0185f27afaefc8a96627273081e1dd4830229057
d4viss <davisonju26@gmail.com>
2023-09-19 00:10:03 -0500

    autenticacion

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -1,7 +1,11 @@
 package med.voll.api.controller;
 
+import io.swagger.annotations.Api;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,15 +21,19 @@ public class AutenticacionController {
 
     private final AuthenticationManager authenticationManager;
 
-    public AutenticacionController(AuthenticationManager authenticationManager) {
+    private TokenService tokenService;
+
+    public AutenticacionController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
-    public ResponseEntity<DatosAutenticacionUsuario> autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
-        Authentication token = new UsernamePasswordAuthenticationToken(
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
+        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(
                 datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var usuario = authenticationManager.authenticate(authenticationToken);
+        String JWTtoken = tokenService.generarToken((Usuario) usuario.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
     }
 }
